c49916f26c07a5d607977ae5ed598a2f84629cb3
ssuruthim <suruthirajalkshmi97@gmail.com>
2022-07-12 12:14:25 +0530

    task complete

--- java script/marriage.js
+++
@@ -1,8 +0,0 @@
-var age=24;
-if(age>=21)
-{
-    console.log("eligible for marriage")
-}
-else{
-    console.log("not eligible")
-}
---
+++ task 6/marriage.js
@@ -0,0 +1,9 @@
+var  femaleage=prompt("female age=");
+var  maleage=prompt("male age=");
+if(femaleage>=21&&maleage>=21)
+{
+    console.log("eligible for marriage")
+}
+else{
+    console.log("not eligible")
+}
